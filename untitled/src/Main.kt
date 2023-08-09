89c6a436a998552e1ed636e1b18f4df595fcb25f
Зиновьев Иван <zinovev.ivan@mail.ru>
2023-08-09 17:21:09 +0300

    123

--- untitled/src/Main.kt
+++ untitled/src/Main.kt
@@ -5,93 +5,82 @@ import java.util.Scanner
 class CoffeeMachine {
     private var scanner = Scanner(System.`in`)
 
-    var water : Int? =null
-    var milk : Int? = null
-    var beans : Int? = null
+    var water: Int? = null
+    var milk: Int? = null
+    var beans: Int? = null
 
     fun start() {
         println("Кофемашина готова к работе")
         while (true) {
             println("Введите команду")
             var inputCommand = scanner.nextLine()
-            when (inputCommand) {
-                "выключить" -> {
-                    println("До свидания!")
-                    break
+            menu(inputCommand)
+
+        }
+    }
+
+    fun menu(i: String) {
+        when (i) {
+            "выключить" -> {
+                println("До свидания!")
+
+            }
+
+            "наполнить" -> {
+                water = 2000
+                milk = 1000
+                beans = 1000
+                println("Ингридиенты пополнены")
+            }
+
+            "статус" -> println("Ингридиентов осталось: ${water} мл воды\n${milk} мл молока\n${beans} гр кофе")
+            "кофе" -> {
+                var inputCommand = scanner.nextLine()
+                coffee(inputCommand)
+
+            }
+
+
+        }
+    }
+
+    fun coffee(i: String) {
+        when (i) {
+            "Эспрессо" -> {
+                if ((checkCoffee(0, 60, 10))) {
+                    water = this.water!!.minus(60)
+                    beans = this.beans!!.minus(10)
+                    println("Эспрессо готов")
                 }
+            }
 
-                "наполнить" -> {
-                    water = 2000
-                    milk = 1000
-                    beans = 1000
-                    println("Ингридиенты пополнены")
-                }
-                "статус" -> println("Ингридиентов осталось: ${water} мл воды\n${milk} мл молока\n${beans} гр кофе")
-                "кофе" -> {
-                    println("Введите название напитка или назад для возврата в главное меню")
-                    inputCommand
-                    when (inputCommand) {
-                        "назад" -> {
-                            start()
-                        }
-                        "Экспрессо" -> {
-                            if (water != null) {
-                                water = water.minus(60)
-                            } else {
-                                println("Недостаточно воды")
-                            }
 
-                        }
-
-                        else -> {
-                            "Рецепт не найден!"
-                        }
-
-                    }
+            "Американо" -> {
+                if (checkCoffee(0, 120, 10)) {
+                    water = this.water!!.minus(120)
+                    beans = this.beans!!.minus(10)
+                    println("Американо готов")
                 }
             }
         }
     }
 
-    fun checkResourses(water : Int, milk : Int, beans: Int) : Boolean {
 
-
-        fun testWater(water: Int): Boolean {
-            if (water < this.water!!) {
-                return false
-            }
+    fun checkCoffee(milk: Int, water: Int, beans: Int): Boolean {
+        if (milk < this.milk!!) {
+            println("Недостаточно молока")
             return false
-        }
-
-        fun  testMilk(milk: Int) : Boolean {
-            if (milk < this.milk!!) {
-                return false
-            }
+        } else
+        if (water < this.water!!) {
+            println("Недостаточно воды")
             return false
-        }
-
-        fun  testbeans(milk: Int) : Boolean {
-            if (beans < this.beans!!) {
-                return false
-            }
+        } else
+        if (beans < this.beans!!) {
+            println("Недостаточно кофе")
             return false
-        }
-
-
-
+        } else return true
     }
-
 }
-
-    class Recepts(name: String, water : Int, beans : Int) {
-        var name : String = name
-        var water : Int = water
-        var beans : Int = beans
-        constructor(name: String, water: Int, beans: Int, milk: Int) : this(name, water, beans){
-            var milk : Int = milk
-        }
-    }
-
 
 // Начните написание программы с публичной функции start().
     // В этот раз мы не даём вам конкретных указаний, как должна быть написана программа.
@@ -101,6 +90,7 @@ class CoffeeMachine {
 
 
 fun main() {
-    var capuchino = Recepts("Капучино", 3000, 50, 1000)
-    println(capuchino.water)
+   var test = CoffeeMachine()
+    test.start()
+
 }
