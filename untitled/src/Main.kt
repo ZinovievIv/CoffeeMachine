ed2e5c1d3e8c2b1f6776883b5ba350cd2be38bd7
Зиновьев Иван <zinovev.ivan@mail.ru>
2023-08-09 18:22:18 +0300

    17 45

--- untitled/src/Main.kt
+++ untitled/src/Main.kt
@@ -5,70 +5,85 @@ import java.util.Scanner
 class CoffeeMachine {
     private var scanner = Scanner(System.`in`)
 
-    var water: Int? = null
-    var milk: Int? = null
-    var beans: Int? = null
+    var water: Int? = 0
+    var milk: Int? = 0
+    var beans: Int? = 0
+
+    var massage : String = " "
+
 
     fun start() {
         println("Кофемашина готова к работе")
         while (true) {
             println("Введите команду")
-            var inputCommand = scanner.nextLine()
+            var inputCommand = (scanner.nextLine()).lowercase()
+
             when (inputCommand) {
                 "выключить" -> {
                     println("До свидания!")
+                    break
                 }
                 "наполнить" -> {
                     water = 2000
                     milk = 1000
-                    beans = 1000
+                    beans = 500
                     println("Ингридиенты пополнены")
                 }
                 "статус" -> println("Ингридиентов осталось: ${water} мл воды\n${milk} мл молока\n${beans} гр кофе")
                 "кофе" -> {
                     println("Введите название напитка или назад для возврата в главное меню")
-                    var inputCommand2 = scanner.nextLine()
+                    var inputCommand2 = (scanner.nextLine()).lowercase()
                     coffee(inputCommand2)
+                }
                 }
             }
         }
-    }
 
 
 
     fun coffee(i: String) {
         when (i) {
             "Эспрессо" -> {
-                if ((checkCoffee(0, 60, 10))) {
+                if ((checkCoffee(0, 60, 10)) == true) {
                     water = this.water!!.minus(60)
                     beans = this.beans!!.minus(10)
                     println("Эспрессо готов")
+                } else {
+                    println(massage)
                 }
             }
 
 
             "Американо" -> {
-                if (checkCoffee(0, 120, 10)) {
+                if (checkCoffee(0, 120, 10) == true) {
                     water = this.water!!.minus(120)
                     beans = this.beans!!.minus(10)
                     println("Американо готов")
+                } else {
+                    println(massage)
                 }
             }
 
             "Капучино" -> {
-                if (checkCoffee(60, 120,20)) {
+                if (checkCoffee(60, 120,20) == true) {
                     water = this.water!!.minus(120)
                     beans = this.beans!!.minus(20)
                     milk = this.milk!!.minus(60)
                     println("Капучино готов")
+                } else {
+                    println(massage)
                 }
             }
 
             "Латте" -> {
-                if (checkCoffee(120,240,20))
-                water = this.water!!.minus(240)
-                beans = this.beans!!.minus(20)
-                milk = this.milk!!.minus(120)
+                if (checkCoffee(120, 240, 20) == true) {
+                    water = this.water!!.minus(240)
+                    beans = this.beans!!.minus(20)
+                    milk = this.milk!!.minus(120)
+                    println("Латте готов")
+                } else {
+                    println(massage)
+                }
             }
 
             "назад" -> {
@@ -76,25 +91,28 @@ class CoffeeMachine {
             }
             else -> {
                 println("Рецепт не найден")
+                var inputCommand3 = (scanner.nextLine()).lowercase()
+                coffee(inputCommand3)
             }
 
         }
     }
 
 
-    fun checkCoffee(milk: Int, water: Int, beans: Int): Boolean {
+    fun checkCoffee(milk: Int, water: Int, beans: Int): Any {
         if (milk > this.milk!!) {
-            println("Недостаточно молока")
-            return false
+            massage = "Недостаточно молока"
+            return massage
         } else
         if (water > this.water!!) {
-            println("Недостаточно воды")
-            return false
+            massage = "Недостаточно воды"
+            return massage
         } else
         if (beans > this.beans!!) {
-            println("Недостаточно кофе")
-            return false
-        } else return true
+            massage = "Недостаточно зерен"
+            return massage
+        }
+        return true
     }
 }
 
@@ -106,5 +124,4 @@ class CoffeeMachine {
 fun main() {
    var test = CoffeeMachine()
     test.start()
-
 }
